4cd6d8bc6531f6eb1820a0888a9e6f2131eb91f7
kunalcoder45 <kunalcoder45@gmail.com>
2025-09-27 10:57:37 +0530

    crud operations

--- src/main/java/com/kunal/publicNews/controller/JournalEntryController.java
+++ src/main/java/com/kunal/publicNews/controller/JournalEntryController.java
@@ -1,53 +1,3 @@
-//package com.kunal.publicNews.controller;
-//
-//
-//import com.kunal.publicNews.entity.JournalEntry;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//@RestController
-//@RequestMapping("/journal")
-//public class JournalEntryController {
-//
-//    public Map<Long,JournalEntry> journalEntries = new HashMap();
-//
-//
-//    @GetMapping
-//    public List<JournalEntry> getAll() {
-//        return new ArrayList<>(journalEntries.values());
-//    }
-//
-//    @PostMapping
-//    public boolean createEntry (@RequestBody JournalEntry myEntries) {
-//        journalEntries.put(myEntries.getId(), myEntries);
-//        return true;
-//    }
-//
-//    @GetMapping("id/{myId}")
-//    public JournalEntry getJournalEntryById(@PathVariable Long myId){
-//        return journalEntries.get(myId);
-//    }
-//
-//    @DeleteMapping("id/{myId}")
-//    public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
-//        return journalEntries.remove(myId);
-//    }
-//
-//
-//    @PutMapping("/id/{id}")
-//    public JournalEntry updateJournalById(@PathVariable Long id, @RequestBody JournalEntry myEntry){
-//        return journalEntries.put(id, myEntry);
-//    }
-//}
-
-
-
-
-
 package com.kunal.publicNews.controller;
 
 
@@ -64,19 +14,35 @@ import java.util.Map;
 public class JournalEntryController {
     public Map<Long,JournalEntry> journalEntries = new HashMap();
 
+
+    // get the all items
     @GetMapping
     public List<JournalEntry> getAll(){
         return new ArrayList<>(journalEntries.values());
     }
 
+    // add items
     @PostMapping
     public boolean createEntries(@RequestBody JournalEntry myEntries){
         journalEntries.put(myEntries.getId(), myEntries);
         return true;
     }
 
-    @GetMapping("/id/{myId}")
-    public JournalEntry updateEntriesById(@PathVariable Long myId){
-        return journalEntries.get(myId);
+    // view items using id
+    @GetMapping("/id/{id}")
+    public JournalEntry readEntriesById(@PathVariable Long id){
+        return journalEntries.get(id);
+    }
+
+    // update values using id
+    @PutMapping("/id/{id}")
+    public JournalEntry updateEntriesById(@PathVariable Long id, @RequestBody JournalEntry myEntries){
+        return journalEntries.put(id, myEntries);
+    }
+
+    // delete content using id
+    @DeleteMapping("/id/{id}")
+    public boolean deleteEntriesById(@PathVariable Long id){
+        return journalEntries.remove(id) != null;
     }
 }
